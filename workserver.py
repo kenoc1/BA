a11de98bfcef2067b80708eafdfd9ac3521aade5
kenoc1 <47333651+kenoc1@users.noreply.github.com>
2022-09-02 14:00:33 +0200

    Update workserver.py

--- workserver.py
+++ workserver.py
@@ -42,12 +42,12 @@ def writebody():
         body += '<br/>Worker thread is running. <a href="./stop_work">Stop work</a><br/>'
 
     cpu_load = psutil.cpu_percent()
-    mem = psutil.virtual_memory()
+    mem = psutil.virtual_memory().used
     mem_load = psutil.virtual_memory().percent
     mem_available = psutil.virtual_memory().available * 100 / psutil.virtual_memory().total
 
   
-    body += '<br/>Activity Monitor:<br/>/CPU = ' + cpu_load + '<br/>/MEM = ' + mem + '<br/>/MEM (load) = ' + mem_load + '<br/>/MEM (available) = ' + mem_available + '<br/>'
+    body += '<br/>Activity Monitor:<br/>/CPU = ' + str(cpu_load) + '<br/>/MEM = ' + str(mem) + '<br/>/MEM (load) = ' + str(mem_load) + '<br/>/MEM (available) = ' + str(mem_available) + '<br/>'
     body += '<br/>Usage:<br/><br/>/do_work = start worker thread<br/>/stop_work = stop worker thread<br/>'
     body += '</h3></ul></body></html>'
     return body
